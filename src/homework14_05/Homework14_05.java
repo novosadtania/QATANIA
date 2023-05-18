cb586b7fe0023e25b84db2e6875b7885df54484e
Tetiana <tanya.novosad.99@gmail.com>
2023-05-18 13:51:03 +0300

    Продовжила домашку

--- src/homework14_05/Homework14_05.java
+++ src/homework14_05/Homework14_05.java
@@ -1,48 +1,55 @@
 package homework14_05;
-import java.util.Date;
-import java.util.Scanner;
+
 public class Homework14_05 {
     public static void main(String[] args) {
 //       1.
-/*
-       Scanner input = new Scanner(System.in);
+        //Я тестую чудово. Що ще потрібно?
+       /* Scanner input = new Scanner(System.in);
+        System.out.println("Введіть строку: ");
+
         String string1 = input.next();
         String string2 = input.next();
         String string3 = input.next();
-        String string4 = input.next();
-        System.out.print(string1 + " " + string2 + " " + string3 + " " + string4 + " ");
+        String string4 = input.next() + " " + input.next() + " " + input.next();
+
+        System.out.println("string1 = " + string1);
+        System.out.println("string2 = " + string2);
+        System.out.println("string3 = " + string3);
+        System.out.println("string4 = " + string4);
 */
 
 //    2.
-    /*Scanner input = new Scanner(System.in);
-        System.out.println("Введіть 3 числа ");
-        int x = input.nextInt();
-        int y = input.nextInt();
-        int w = input.nextInt();
-        if (x <= 0) {
-            System.out.println("Трикутник скласти неможливо");
-        } else if (y <= 0) {
-            System.out.println("Трикутник скласти неможливо");
-        } else if (w <= 0) {
-            System.out.println("Трикутник скласти неможливо");
-        } else if (x >= 1) {
-            System.out.println("Трикутник скласти можливо");
-        } else if (y >= 1) {
-            System.out.println("Трикутник скласти можливо");
-        } else if (w >= 1) {
-            System.out.println("Трикутник скласти можливо");
+   /* Scanner input = new Scanner(System.in);
+         Scanner in = new Scanner(System.in);
+        System.out.println("Введіть сторону трикутника:");
+        int a = in.nextInt();
+        System.out.println("Введіть сторону трикутника:");
+        int b = in.nextInt();
+        System.out.println("Введіть сторону трикутника:");
+        int c = in.nextInt();
+
+        if ((a + b > c) && (a + c > b) && (b + c > a)){
+            System.out.println("Із цих сторін можна побудувати трикутник");
+        } else {
+            System.out.println("Із цих сторін не можна побудувати трикутник");
         }*/
 
 //     3
-      /*  Date date = new Date();
-        Scanner input = new Scanner(System.in);
-        System.out.println("Введіть рік народження : ");
-        int d = input.nextInt();
-        int a = 2023 - d;
-        System.out.println("Ваш вік : " + a );*/
+       /* Scanner in = new Scanner(System.in);
+        System.out.println("Введіть рік вашого народження:");
+        int yearOfBirth = in.nextInt();
+        Date date = new Date();
+
+        System.out.println(date);
+
+        String actualYear = String.format("%tY", date);
+
+        int actualYearInt = Integer.parseInt(actualYear);
+        System.out.println("Ваш вік : " + (actualYearInt - yearOfBirth));*/
+
 
 //        4
-           /* Scanner input = new Scanner(System.in);
+          /*  Scanner input = new Scanner(System.in);
             System.out.println("Введіть  ваше число ");
             int x = input.nextInt();
             System.out.println("Введіть ваше 2 число ");
@@ -75,7 +82,74 @@ public class Homework14_05 {
 
 //        6
 
+
+
+//7.
+       /* Scanner in = new Scanner(System.in);
+        System.out.println("Введіть вашу ОС: LiNux, macOS, Windows");
+        String os = in.nextLine();
+        String programm;
+        switch (os.toUpperCase()) {
+            case ("LINUX"):
+                System.out.println("Введіть вашу програму: IntelliJ IDEA, Git, Java");
+                programm = in.nextLine();
+                switch (programm.toLowerCase()) {
+                    case ("intellij"):
+                        System.out.println("Ваше посилання на intellij, linux : https://www.jetbrains.com/ru-ru/idea/download/ ");
+                        break;
+                    case ("git"):
+                        System.out.println("Ваше посилання на git, linux : https://git-scm.com/ ");
+                        break;
+                    case ("java"):
+                        System.out.println("Ваше посилання на java, linux : https://www.oracle.com/cis/java/technologies/downloads/#java17 ");
+                        break;
+                    default:
+                        System.out.println("Такої програми мы не знайшли");
+                }
+                break;
+            case ("MACOS"):
+                System.out.println("Введіть вашу програму: IntelliJ IDEA, Git, Java");
+                programm = in.nextLine();
+                switch (programm.toLowerCase()) {
+                    case ("intellij"):
+                        System.out.println("Ваше посилання на intellij, macos : https://www.jetbrains.com/ru-ru/idea/download/ ");
+                        break;
+                    case ("git"):
+                        System.out.println("Ваше посилання на git, macos : https://git-scm.com/ ");
+                        break;
+                    case ("java"):
+                        System.out.println("Ваше посилання на java, macos : https://www.oracle.com/cis/java/technologies/downloads/#jdk17-mac");
+                        break;
+                    default:
+                        System.out.println("Такої програми мы не знайшли");
+                }
+
+
+                break;
+            case ("WINDOWS"):
+                System.out.println("Введіть вашу програму: IntelliJ IDEA, Git, Java");
+                programm = in.nextLine();
+                switch (programm.toLowerCase()) {
+                    case ("intellij"):
+                        System.out.println("Ваше посилання  на intellij, windows : https://www.jetbrains.com/ru-ru/idea/download/ ");
+                        break;
+                    case ("git"):
+                        System.out.println("Ваше посилання на git, windows : https://git-scm.com/ ");
+                        break;
+                    case ("java"):
+                        System.out.println("Ваше посилання на java, windows : https://www.oracle.com/cis/java/technologies/downloads/#jdk17-windows ");
+                        break;
+                    default:
+                        System.out.println("Такої програми ми не знайшли");
+                }
+
+                break;
+            default:
+                System.out.println("Такої ОС не знайдено");
+                */
     }
+
+}
 
 }
 
