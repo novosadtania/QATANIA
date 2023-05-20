a5cf1e1eb8ffe4228294cf1ea1c16d721cde9144
Tetiana <tanya.novosad.99@gmail.com>
2023-05-20 18:14:36 +0300

    .

--- src/homeworkLesson5/Homework5.java
+++ src/homeworkLesson5/Homework5.java
@@ -1,4 +1,15 @@
 package homeworkLesson5;
-
+/*Есть одномерный массив из 10 элементов, заполнен-
+        ный случайными числами. Пользователь вводит с клавиатуры
+        число. Программа показывает есть ли такое число в созданном
+        до этого массиве.*/
 public class Homework5 {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++){
+            array[i] = (int) (Math.random());
+            System.out.println(array);
+
+    }
+    }
 }
