11ce03cba7fd7993250213e6800c104b511df91e
Tetiana <tanya.novosad.99@gmail.com>
2023-05-20 16:26:28 +0300

    .

--- src/homeworkLesson5/Homework1.java
+++ src/homeworkLesson5/Homework1.java
@@ -1,4 +1,26 @@
 package homeworkLesson5;
 
+import java.util.Scanner;
+
 public class Homework1 {
+    /*1)Написать программу, которая будет считывать введенные пользователем
+  слова с клавиатуры слова, и склеивать их в одно предложение до тех пор,
+  пока пользователь не введет с клавитуры слово STOP.
+  Все слова введенные до этого должны отобразится в консоли
+  одним предложением.*/
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String result = "";
+        while (true) {
+            System.out.println("Введіть ваше слово(для зупинки напишіть STOP)");
+            String input = scanner.nextLine();
+            if (input.equals("STOP")) {
+                break;
+            }
+            result = result + " " + input;
+            System.out.println(result);
+
+        }
+        System.out.println(result);
+    }
 }
