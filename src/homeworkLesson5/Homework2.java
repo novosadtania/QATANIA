0d7e169657ca7400ce70634aa010febc89e8799a
Tetiana <tanya.novosad.99@gmail.com>
2023-05-20 17:38:08 +0300

    .

--- src/homeworkLesson5/Homework2.java
+++ src/homeworkLesson5/Homework2.java
@@ -1,12 +1,15 @@
 package homeworkLesson5;
 
 import java.util.Scanner;
-
+/*2)Написать программу в которой пользователь вводит с клавиатуры число,
+        а программа определяет, является она полиндромом или нет.
+        И выводит данную информацию на экран.*/
 public class Homework2 {
     public static void main(String[] args) {
         Scanner scanerNumber = new Scanner(System.in);
 
-        System.out.println("Введіть число : "); int lastDigit, sum = 0, a;
+        System.out.println("Введіть число : ");
+        int lastDigit, sum = 0, a;
         int inputNumber = scanerNumber.nextInt();
         a = inputNumber;
 
