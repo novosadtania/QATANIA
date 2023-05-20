91816441c43d8e3efa16eb211c2cf0800c0396e4
Tetiana <tanya.novosad.99@gmail.com>
2023-05-20 16:26:53 +0300

    .

--- src/taniaForMe/QATania.java
+++ src/taniaForMe/QATania.java
@@ -1,16 +1,11 @@
 package taniaForMe;
 
 import java.util.Scanner;
+
 public class QATania {
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        System.out.println("Введіть  ваше число ");
-        int x = input.nextInt();
-        System.out.println("Введіть ваше 2 число ");
-        int y = input.nextInt();
-        boolean result = x > y? true : false;
-        String string = "Число " + x + " більше за "  + y +" і це " + result;
-        System.out.print(string);
+    public static void main(String[] args) {
 
     }
+
 }
+
