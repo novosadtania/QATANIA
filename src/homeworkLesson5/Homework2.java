83642d04703de739f0464b918482b867b6ea9f19
Tetiana <tanya.novosad.99@gmail.com>
2023-05-20 16:33:19 +0300

    .

--- src/homeworkLesson5/Homework2.java
+++ src/homeworkLesson5/Homework2.java
@@ -4,21 +4,20 @@ import java.util.Scanner;
 
 public class Homework2 {
     public static void main(String[] args) {
+        Scanner scanerNumber = new Scanner(System.in);
 
-    }
-    Scanner scanerNumber = new Scanner(System.in);
-        System.out.println("Введіть число : ");
-    int lastDigit, sum = 0, a;
-    int inputNumber = scanerNumber.nextInt();
-    a = inputNumber;
+        System.out.println("Введіть число : "); int lastDigit, sum = 0, a;
+        int inputNumber = scanerNumber.nextInt();
+        a = inputNumber;
 
         while (a > 0) {
-        lastDigit = a % 10;
-        sum = (sum * 10) + lastDigit;
-        a = a / 10;
-    }
+            lastDigit = a % 10;
+            sum = (sum * 10) + lastDigit;
+            a = a / 10;
+        }
         if (sum == inputNumber)
             System.out.println("Number is palindrome ");
         else
-                System.out.println("Number is not palindrome");
+            System.out.println("Number is not palindrome");
+    }
 }
