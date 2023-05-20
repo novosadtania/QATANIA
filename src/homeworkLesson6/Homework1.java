ec2135907d2fcdbfc5403da5cd16e8cbaad6716d
Tetiana <tanya.novosad.99@gmail.com>
2023-05-21 00:02:31 +0300

    .

--- src/homeworkLesson6/Homework1.java
+++ src/homeworkLesson6/Homework1.java
@@ -1,26 +1,47 @@
 package homeworkLesson6;
+/*Написати програму "числа", яка загадує випадкове число та пропонує гравцеві його вгадати.
 
+        Технічні вимоги:
+
+        За допомогою java.util.Random програма загадує випадкове число в діапазоні[0-100]
+        та пропонує гравцеві через консоль ввести своє ім'я, яке зберігається в змінній name.
+        Перед початком на екран виводиться текст: Let the game begin!
+
+        Сам процес гри обробляється у нескінченному циклі.
+        Гравцеві пропонується ввести число в консоль, після чого програма порівнює загадану
+        кількість з тим, що ввів користувач.
+        Якщо введене число менше загаданого, програма виводить на екран текст:
+        Your number is too small. Please, try again.. Якщо введене число більше за загадане,
+        то програма виводить на екран текст: Your number is too big. Please, try again..
+        Якщо введене число відповідає загаданому, то програма виводить текст: Congratulations, {name}!
+
+        Завдання повинно бути виконане за допомогою масиви (НЕ використовуйте інтерфейси List, Set, Map).*/
+
+import java.util.Random;
 import java.util.Scanner;
 
 public class Homework1 {
-    /*1)Написать программу, которая будет считывать введенные пользователем
-   слова с клавиатуры слова, и склеивать их в одно предложение до тех пор,
-   пока пользователь не введет с клавитуры слово STOP.
-   Все слова введенные до этого должны отобразится в консоли
-   одним предложением.*/
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String result = "";
-        while (true) {
-            System.out.println("Введіть ваше слово(для зупинки напишіть STOP)");
-            String input = scanner.nextLine();
-            if (input.equals("STOP")) {
-                break;
-            }
-            result = result + " " + input;
-            System.out.println(result);
-
+        Random random = new Random();
+        System.out.println("Write your name : ");
+        String name = scanner.nextLine();
+        System.out.println("Let the game begin," + name);
+        System.out.println("Введіть число від 0 до 100");
+        int a = scanner.nextInt();
+        int u = random.nextInt(100);
+        int[] arrayInt = new int[100];
+        System.out.println("Рандомне число : " + u);
+        int result = 0;
+        for (int i = 0; i < arrayInt.length; i++) {
+            result = result + arrayInt[i];
         }
-        System.out.println(result);
+        if (a < u) {
+            System.out.println("Your number is too small. Please, try again.. ");
+        } else if (a > u) {
+            System.out.println("Your number is too big. Please, try again..");
+        } else {
+            System.out.println("Congratulations," + name);
         }
     }
+}
