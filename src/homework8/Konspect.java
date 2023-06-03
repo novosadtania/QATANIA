2b1f60809577491cb09d34146b1e3afaf3538e3f
Tetiana <tanya.novosad.99@gmail.com>
2023-06-03 15:08:58 +0300

    Домашка

---
+++ src/homework8/Konspect.java
@@ -0,0 +1,17 @@
+package homework8;
+/*3) Создать класс Конспект. Класс должен содержать следующие поля:
+         Название предмета;
+         ФИО студента;
+         Количество страниц;
+         Год выпуска;
+         Цвет обложки.
+         Название заведения, где учится студент;*/
+public class Konspect {
+    public String nameOfPredmet;
+    public String fIOStudent;
+    public int numberOfPages;
+    public int graduationYear;
+    public String coverColor;
+    public String school;
+
+}
