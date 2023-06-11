bf94e2b0217ccfbd9d4bc5a2843b82da9e687393
Tetiana <tanya.novosad.99@gmail.com>
2023-06-11 15:53:24 +0300

    Домашка

--- src/homework10/Main.java
+++ src/homework10/Main.java
@@ -2,7 +2,5 @@ package homework10;
 
 public class Main {
     public static void main(String[] args) {
-        Human human = new Human("Tyu","FFf", 444,33, (new Pet("Dog", "Dag", 3, 44, new String[]{"Eat", "FFrfgd"})), "Moth", "Dad");
-        System.out.println(human.toString());
     }
 }
