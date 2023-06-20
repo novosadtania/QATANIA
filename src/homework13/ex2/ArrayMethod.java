91f5db226cc19ef60e78173dd71c42ca6098a013
Tetiana <tanya.novosad.99@gmail.com>
2023-06-20 19:49:30 +0300

    Домашка

---
+++ src/homework13/ex2/ArrayMethod.java
@@ -0,0 +1,11 @@
+package homework13.ex2;
+
+import java.util.Random;
+
+public class ArrayMethod {
+    int[] array = new int[30];
+
+    public void arrayMethod(){
+        array[30] = (int) (Math.random() * 31);
+    }
+}
